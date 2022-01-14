19c2282bc978494873d157a29bf09e9b0f5473cb
Jorge Zupirolli <jorgebertolo@gmail.com>
2022-01-14 17:59:05 -0400

    Desenvolvido funcao fetchProducts

--- helpers/fetchProducts.js
+++ helpers/fetchProducts.js
@@ -1,5 +1,15 @@
-const fetchProducts = () => {
-  // seu código aqui
+const getURL = (query) =>
+  `https://api.mercadolibre.com/sites/MLB/search?q=${query}`;
+
+const fetchProducts = async (query) => {
+  try {
+    const url = getURL(query);
+    const response = await fetch(url);
+    const data = response.json();
+    return data;
+  } catch (error) {
+    return error;
+  }
 };
 
 if (typeof module !== 'undefined') {
