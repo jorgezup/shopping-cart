ec50c1cde157ac59d16a4c291dc274802a0d2579
Jorge Zupirolli <jorgebertolo@gmail.com>
2022-01-15 14:06:21 -0400

    add: create an function fetchItem, rececive an itemId and returns its object

--- helpers/fetchItem.js
+++ helpers/fetchItem.js
@@ -1,5 +1,14 @@
-const fetchItem = () => {
-  // seu código aqui
+const getURLItem = (itemId) => `https://api.mercadolibre.com/items/${itemId}`;
+
+const fetchItem = async (itemId) => {
+  try {
+    const url = getURLItem(itemId);
+    const response = await fetch(url);
+    const data = response.json();
+    return data;
+  } catch (error) {
+    return error;
+  }
 };
 
 if (typeof module !== 'undefined') {
