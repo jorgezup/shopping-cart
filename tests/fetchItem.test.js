88e6cd5477b263450f634bdf68719962af2f6000
Jorge Zupirolli <jorgebertolo@gmail.com>
2022-01-15 14:05:28 -0400

    test: create an test for fetchItem

--- tests/fetchItem.test.js
+++ tests/fetchItem.test.js
@@ -3,6 +3,26 @@ const { fetchItem } = require('../helpers/fetchItem');
 const item = require('../mocks/item');
 
 describe('2 - Teste a função fecthItem', () => {
-  // implemente seus testes aqui
-  fail('Teste vazio');
+  it('Should be a function', () => {
+    expect(typeof fetchItem).toBe('function');
+  });
+  it('Should verify whether fetch has been called', async () => {
+    await fetchItem('MLB1615760527');
+    expect(fetch).toHaveBeenCalled();
+  });
+  it('Should receive an parameter "MLB1615760527" and verify whether the function has been called correctly', async () => {
+    const itemId = 'MLB1615760527';
+    const endpoint = `https://api.mercadolibre.com/items/${itemId}`;
+    await fetchItem(itemId);
+
+    expect(fetch).toHaveBeenCalledWith(endpoint);
+  });
+  it('Should receive an structure and verify wheter this structure is the same as "item"', async () => {
+    const response = await fetchItem('MLB1615760527');
+
+    expect(response).toMatchObject(item);
+  });
+  it('Should returns an error message wheter the function has been called without parameters', async () => {
+    expect(await fetchItem()).toEqual(new Error('You must provide an url'));
+  });
 });
