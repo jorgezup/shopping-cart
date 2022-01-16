3fd970a6a6ee3a6ea70ec68fbfc2f3b8a7b62619
Jorge Zupirolli <jorgebertolo@gmail.com>
2022-01-16 15:52:16 -0400

    add: create an function to save Items in local storage

--- helpers/saveCartItems.js
+++ helpers/saveCartItems.js
@@ -1,5 +1,14 @@
-const saveCartItems = () => {
-  // seu código aqui
+const saveCartItems = (item) => {
+  const cartItems = localStorage.getItem('cartItems');
+
+  if (!cartItems) {
+    const cartArray = [];
+    cartArray.push(item);
+    localStorage.setItem('cartItems', JSON.stringify(cartArray));
+  }
+  const arrayOfItems = JSON.parse(cartItems);
+  arrayOfItems.push(item);
+  localStorage.setItem('cartItems', JSON.stringify(arrayOfItems));
 };
 
 if (typeof module !== 'undefined') {
