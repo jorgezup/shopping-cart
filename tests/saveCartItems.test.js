dd512500b900eeeecdcc3fcdd52c3f530e2265fb
Jorge Zupirolli <jorgebertolo@gmail.com>
2022-01-18 18:49:40 -0400

    Requisito 10

--- tests/saveCartItems.test.js
+++ tests/saveCartItems.test.js
@@ -4,6 +4,16 @@ const saveCartItems = require('../helpers/saveCartItems');
 localStorageSimulator('setItem');
 
 describe('4 - Teste a função saveCartItems', () => {
-  // implemente seus testes aqui
-  fail('Teste vazio');
+  it('Testa se ao executar a função com o argumento especificado, o método localStorage.setItem é chamado', () => {
+    saveCartItems('<ol><li>Item</li></ol>');
+    expect(localStorage.setItem).toHaveBeenCalled();
+  });
+  it('Testa se ao executar a função com o argumento especificado, o método localStorage.setItem é chamado com o parâmetro cartItems e o valor passado', () => {
+    const item = '<ol><li>Item</li></ol>';
+    saveCartItems(item);
+    expect(localStorage.setItem).toHaveBeenCalledWith(
+      'cartItems',
+      JSON.stringify(item),
+    );
+  });
 });
