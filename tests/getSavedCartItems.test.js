6064aadc13938d03e2328f6c5968db19c544ea9d
Jorge Zupirolli <jorgebertolo@gmail.com>
2022-01-19 16:33:51 -0400

    Requisito 11

--- tests/getSavedCartItems.test.js
+++ tests/getSavedCartItems.test.js
@@ -4,6 +4,12 @@ const getSavedCartItems = require('../helpers/getSavedCartItems');
 localStorageSimulator('getItem');
 
 describe('4 - Teste a função getSavedCartItems', () => {
-  // implemente seus testes aqui
-  fail('Teste vazio');
+  it('Should call getSavedCartItems using the method getItem', () => {
+    getSavedCartItems('cartItems');
+    expect(localStorage.getItem).toHaveBeenCalled();
+  });
+  it('Should call getSavedCartItems using the method getItem and passing cartItens as a parameter', () => {
+    getSavedCartItems('cartItems');
+    expect(localStorage.getItem).toHaveBeenCalledWith('cartItems');
+  });
 });
