4b670b7b3250951d5d595c33bde7647cf153def4
Jorge Zupirolli <jorgebertolo@gmail.com>
2022-01-19 16:33:43 -0400

    refactor: Requisito 10

--- tests/saveCartItems.test.js
+++ tests/saveCartItems.test.js
@@ -4,11 +4,12 @@ const saveCartItems = require('../helpers/saveCartItems');
 localStorageSimulator('setItem');
 
 describe('4 - Teste a função saveCartItems', () => {
-  it('Testa se ao executar a função com o argumento especificado, o método localStorage.setItem é chamado', () => {
-    saveCartItems('<ol><li>Item</li></ol>');
+  it('Should runs saveCartItems with an argument and expect that localStorage with setItem haveBeenCalled', () => {
+    const item = '<ol><li>Item</li></ol>';
+    saveCartItems(item);
     expect(localStorage.setItem).toHaveBeenCalled();
   });
-  it('Testa se ao executar a função com o argumento especificado, o método localStorage.setItem é chamado com o parâmetro cartItems e o valor passado', () => {
+  it('Should runs saveCartItems with an argument and expect that localStorage with setItem haveBeenCalledWith "cartItems" and the item ', () => {
     const item = '<ol><li>Item</li></ol>';
     saveCartItems(item);
     expect(localStorage.setItem).toHaveBeenCalledWith(
